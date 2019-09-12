bafac4a940fdc1b82e45ebf26078bae9081d22af
Mohamed Abdulle <yarae99@gmail.com>
2019-09-11 22:09:36 -0500

    Updates

---
+++ precache-manifest.c330fbd7bd6f8806d571429c2cd08fee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75fb225b1a4f98179e209230b6b3d5e",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "f28887ebc8383684e40a",
+    "url": "/recipe-app/static/css/main.1dbbab35.chunk.css"
+  },
+  {
+    "revision": "00eb65cb74e712c77cb4",
+    "url": "/recipe-app/static/js/2.59eb6971.chunk.js"
+  },
+  {
+    "revision": "f28887ebc8383684e40a",
+    "url": "/recipe-app/static/js/main.29f189cf.chunk.js"
+  },
+  {
+    "revision": "2cdc37c46ee7a3de3f17",
+    "url": "/recipe-app/static/js/runtime~main.2c4ead4e.js"
+  }
+]);
